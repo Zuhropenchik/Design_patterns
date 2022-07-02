e0c2d062b85e15ab8366e2c1fcbc1704350c01ce
Zakhar Urvancev <92048210+Zuhropenchik@users.noreply.github.com>
2022-07-02 20:49:32 +0300

    Update Adapter.cpp

--- Structural/Adapter.cpp
+++ Structural/Adapter.cpp
@@ -1,1 +1,49 @@
+#include<iostream>
 
+class ITransport {
+public:
+    virtual void drive() = 0;
+};
+
+class Car : public ITransport {
+public:
+    void drive() override {
+        std::cout << "Car is driving\n";
+    }
+};
+
+class Horse {
+public:
+    void walk() {
+        std::cout << "Horse is walking\n";
+    }
+};
+
+class People { //Client
+public:
+    void ride(ITransport &transport) {
+        transport.drive();
+    }
+};
+
+class Saddle : public ITransport {
+public:
+    explicit Saddle(Horse horse) : _horse(horse) {}
+
+    void drive() override {
+        _horse.walk();
+    }
+
+private:
+    Horse _horse;
+};
+
+int main() {
+    People man;
+    Car BMW;
+    man.ride(BMW);
+    Horse some_horse;
+    Saddle saddle_for_horse(some_horse);
+    man.ride(saddle_for_horse);
+    return 0;
+}
