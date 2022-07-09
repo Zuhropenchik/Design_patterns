d9fae6d652d9ba69a79f43a015379e4a4c60ee04
Zakhar Urvancev <92048210+Zuhropenchik@users.noreply.github.com>
2022-07-09 22:00:19 +0300

    Update Facade.cpp

--- Structural/Facade.cpp
+++ Structural/Facade.cpp
@@ -1,50 +1,58 @@
 #include<iostream>
 
-class WebSocketManager{
+class WebSocketManager {
 public:
-    void put_request(const std::string &message){
+    void put_request(const std::string &message) {
         std::cout << "To server: " << message << '\n';
     }
-    void get_request(const std::string &message){
+
+    void get_request(const std::string &message) {
         std::cout << "From server: " << message << '\n';
     }
 };
-class DrawingManager{
+
+class DrawingManager {
 public:
-        void create_rectangle(){
-            std::cout << "Rectangle is creating\n";
-        }
-        void create_square(){
-            std::cout << "Square is creating\n";
-        }
+    void create_rectangle() {
+        std::cout << "Rectangle is creating\n";
+    }
+
+    void create_square() {
+        std::cout << "Square is creating\n";
+    }
 };
-class UI{
+
+class UI {
 public:
-    void draw(){
+    void draw() {
         std::cout << "Drawing current canvas\n";
     }
 };
-class LiveOnCanvas{ //Facade
+
+class LiveOnCanvas { //Facade
 public:
-    LiveOnCanvas(const UI & ui, const DrawingManager & dm, const WebSocketManager & wsm): _ui(ui), 
-    _dm(dm), _wsm(wsm){}
-    void create_rectangle(){
+    LiveOnCanvas(const UI &ui, const DrawingManager &dm, const WebSocketManager &wsm) : _ui(ui),
+                                                                                        _dm(dm), _wsm(wsm) {}
+
+    void create_rectangle() {
         _wsm.put_request("Creating rectangle");
         _wsm.get_request("Success");
         _dm.create_rectangle();
         _ui.draw();
     }
+
 private:
     UI _ui;
     DrawingManager _dm;
     WebSocketManager _wsm;
-    
+
 };
+
 int main() {
     UI ui;
     DrawingManager dm;
     WebSocketManager wsm;
-    LiveOnCanvas for_egor(ui, dm,wsm);
+    LiveOnCanvas for_egor(ui, dm, wsm);
     for_egor.create_rectangle();
     return 0;
 }
